901ce939a195361f23b6d307398d1c8f454e943b
Javier Guijarro <javier.unix@gmail.com>
2017-03-22 11:37:09 +0100

    word occurrences

---
+++ ini6.py
@@ -0,0 +1,12 @@
+# program for counting the number od ocurrencies of a word in a text
+
+sequence_of_sentences = ['When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be']
+
+from collections import Counter
+
+counts = Counter()
+
+for sentence in sequence_of_sentences:
+    counts.update(word.strip('.,?!"\'') for word in sentence.split()) # split the words in the text
+for key, value in counts.items():
+	print key, value
